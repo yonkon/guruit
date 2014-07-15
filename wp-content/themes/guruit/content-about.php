b21b82242c5fc8b4f6565c187e070dc26e19f340
Shikon <yonkon.ru@gmail.com>
2014-07-15 03:23:09 +0300

    страница веб разработки

--- wp-content/themes/guruit/content-about.php
+++ wp-content/themes/guruit/content-about.php
@@ -1,6 +1,18 @@
 <div id="center_content">
 	<div id="banner_header">
-
+		<div id="services_list_container">
+			<ul id="services_list">
+				<li class="site_development_service">Вэб программирование</li>
+				<li class="site_development_service">Системное программирование</li>
+				<li class="site_development_service">Графический дизайн</li>
+				<li class="site_development_service">Верстка сайтов любой сложности</li>
+				<li class="site_development_service">Сайт "под ключ"</li>
+				<li class="site_development_service">SMM</li>
+				<li class="site_development_service">Продвижение</li>
+				<li class="site_development_service">Вирусна реклама</li>
+				<li class="site_development_service">Контент менеджмент</li>
+			</ul>
+		</div>
 	</div>
 </div>
 <?php
